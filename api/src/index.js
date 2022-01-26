e1457023598ca35f7545aff9d9bb7625d29468c2
German Martusevich <german.martusevich@easybrain.com>
2022-01-26 16:55:16 +0300

    add mongodb

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
+const { connectDb } = require("./helpers/db");
+const { host, port } = require("./configuration");
 const app = express();
+
+const startServer = () => {
+    app.listen(3000, () => {
+        console.log('Started api service on port: ${port}');
+        console.log('On host ${host}');
+    });
+}
 
 app.get("/test", (req, res) => {
     res.send("Server work!");
 });
 
-app.listen(3000, () => {
-    console.log("Started api service");
-});
+
+connectDb()
+    .on("error", console.log)
+    .on("disconnected", connectDb)
+    .once("open", startServer);
